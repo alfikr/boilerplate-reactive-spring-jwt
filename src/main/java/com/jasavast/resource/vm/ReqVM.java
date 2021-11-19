3e61fa93b68ba3a39f86728b853d180aa2e49afb
Danang <daffodil1411@gmail.com>
2021-11-19 09:12:52 +0700

    add validation at data

--- src/main/java/com/jasavast/resource/vm/ReqVM.java
+++ src/main/java/com/jasavast/resource/vm/ReqVM.java
@@ -4,11 +4,13 @@ import lombok.Data;
 import org.json.JSONObject;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Data
 public class ReqVM {
     @NotBlank
     private String method;
+    @NotNull
     private Object data;
 
     public JSONObject toJson(){
